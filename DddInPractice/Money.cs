d156c8be44ecaf8597751d57ddd98ea3c105d48d
Jurgen Azofeifa <azofeifaj@autonation.com>
2020-05-28 16:42:18 -0600

    Add project files.

---
+++ DddInPractice/Money.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DddInPractice.Logic
+{
+    public sealed class Money
+    {
+        #region System Money Count
+        public int OneCentCount { get; set; }
+        public int TenCentCount { get; set; }
+        public int QuarterCentCount { get; set; }
+        public int OneDollarCount { get; set; }
+        public int FiveDollarCount { get; set; }
+        public int TwentyDollarCount { get; set; }
+        #endregion
+
+        public Money(int oneCentCount, int tenCentCount, int quarterCentCount,
+            int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            OneCentCount += oneCentCount;
+            TenCentCount += tenCentCount;
+            QuarterCentCount += quarterCentCount;
+            OneDollarCount += oneDollarCount;
+            FiveDollarCount += fiveDollarCount;
+            TwentyDollarCount += twentyDollarCount;
+            ;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCentCount + money2.QuarterCentCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount
+                );
+
+            return sum;
+        }
+    }
+}
---
+++ DddInPractice/SnackMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DddInPractice.Logic
+{
+    public sealed class SnackMachine
+    {
+        //sealed keyword used to restrict the inheritance feature of object oriented programming
+        //inserting separate properties for the different coins and bills, keeps track of the exact amount of each one
+
+        //This replaces using Money's properties twice for syste and transaction 
+        public Money MoneyInside { get; set; }
+        public Money MoneyInTransaction { get; set; }
+
+        public void InsertMoney(Money money)
+        {
+            MoneyInTransaction += money;
+        }
+
+        public void ReturnMoney()
+        {
+            //MoneyInTransaction = 0;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransaction;
+            //MoneyInTransaction = 0;
+        }
+    }
+}
